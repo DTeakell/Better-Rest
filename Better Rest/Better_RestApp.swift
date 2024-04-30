b55321ad4f744959be6773ea44c6f0d5ddee7ec5
Dillon Teakell <teakelld@icloud.com>
2024-04-30 10:29:30 -0400

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Dillon Teakell on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
